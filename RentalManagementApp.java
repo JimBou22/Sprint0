20fe83ce9847e793388482a8bc252807bf8ebfbc
JimBou22 <60979058+JimBou22@users.noreply.github.com>
2020-02-12 12:17:06 -0800

    Added functionality
    Location name and Available vehicles

--- RentalManagementApp.java
+++ RentalManagementApp.java
@@ -1,4 +1,5 @@
-package ijs_sprint1;
+package IJS_Sprint1;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -54,24 +55,24 @@ public class RentalManagementApp {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+
 		frmRentalLocationManager = new JFrame();
 		frmRentalLocationManager.setTitle("Rental Location Manager");
 		frmRentalLocationManager.setBounds(100, 100, 800, 500);
 		frmRentalLocationManager.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		frmRentalLocationManager.getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
+
 		tblLocations = new JTable();
 		scrollPane.setViewportView(tblLocations);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		frmRentalLocationManager.setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mntmExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -80,47 +81,53 @@ public class RentalManagementApp {
 			}
 		});
 		mnFile.add(mntmExit);
-		
+
 		JMenu mnLocation = new JMenu("Location");
 		menuBar.add(mnLocation);
-		
+
 		JMenuItem mntmNewLocation = new JMenuItem("New Location");
 		mntmNewLocation.addActionListener(e -> {
 			newLocation(locationList);
 		});
 		mnLocation.add(mntmNewLocation);
-		
+
 		JMenu mnLookupLocations = new JMenu("Lookup Locations");
 		mnLocation.add(mnLookupLocations);
-		
+
 		JMenuItem mntmByZipCode = new JMenuItem("by ZIP Code");
 		mnLookupLocations.add(mntmByZipCode);
-		
+
 		JMenuItem mntmByName = new JMenuItem("by Name");
 		mnLookupLocations.add(mntmByName);
-		
+
 		JMenu mnQueries = new JMenu("Queries");
 		menuBar.add(mnQueries);
-		
+
 		JMenu mnLookupRentalRates = new JMenu("Lookup Rental Rates");
 		mnQueries.add(mnLookupRentalRates);
-		
+
 		JMenuItem mntmByLocationName = new JMenuItem("by Location Name");
+		mntmByLocationName.addActionListener(e -> {
+			// Filter rates by a certain Location Name
+			ratesByLoc(locationList);
+		});
 		mnLookupRentalRates.add(mntmByLocationName);
-		
+
 		JMenuItem mntmAvailableVehicles = new JMenuItem("Available Vehicles");
 		mntmAvailableVehicles.addActionListener(e -> {
-			// Do stuff
+			// DO stuff
+			// Complete list, pull availableVehicle for each location
+			availableV(tblLocations, locationList);
 		});
 		mnQueries.add(mntmAvailableVehicles);
-		
+
 		JMenu mnCalculate = new JMenu("Calculate");
 		menuBar.add(mnCalculate);
-		
+
 		JMenuItem mntmDailyRevenueFor = new JMenuItem("Daily Revenue for Location");
 		mnCalculate.add(mntmDailyRevenueFor);
 	}
-	
+
 	private void newLocation(List<RentalLocations> list) {
 //		JFrame frmNewLocation = new JFrame("New Location");
 //		frmNewLocation.setSize(new Dimension(500, 300));
@@ -131,23 +138,23 @@ public class RentalManagementApp {
 		c.ipadx = 5;
 		c.ipady = 10;
 //		frmNewLocation.add(pnlNewLocation);
-		
+
 		JLabel lblName = new JLabel("Location Name:");
 		c.gridx = 0;
 		c.gridy = 0;
 		pnlNewLocation.add(lblName, c);
-		
+
 		JTextField txtName = new JTextField(20);
 		c.gridx = 1;
 		c.gridy = 0;
 		pnlNewLocation.add(txtName, c);
-		
+
 		JLabel lblNameErr = new JLabel();
 		lblNameErr.setForeground(Color.RED);
 		c.gridx = 2;
 		c.gridy = 0;
 		pnlNewLocation.add(lblNameErr, c);
-		
+
 //		JLabel lblRate = new JLabel("Rental Rate:");
 //		c.gridx = 0;
 //		c.gridy = 1;
@@ -163,59 +170,60 @@ public class RentalManagementApp {
 //		c.gridx = 2;
 //		c.gridy = 1;
 //		pnlNewLocation.add(lblRateErr, c);
-		
+
 		JLabel lblVehicles = new JLabel("# of Rented Vehicles:");
 		c.gridx = 0;
 		c.gridy = 2;
 		pnlNewLocation.add(lblVehicles, c);
-		
+
 		JTextField txtVehicle = new JTextField(20);
 		c.gridx = 1;
 		c.gridy = 2;
 		pnlNewLocation.add(txtVehicle, c);
-		
+
 		JLabel lblVehiclesErr = new JLabel();
 		lblVehiclesErr.setForeground(Color.RED);
 		c.gridx = 2;
 		c.gridy = 2;
 		pnlNewLocation.add(lblVehiclesErr, c);
-		
+
 		JLabel lblZip = new JLabel("ZIP Code:");
 		c.gridx = 0;
 		c.gridy = 3;
 		pnlNewLocation.add(lblZip, c);
-		
+
 		JTextField txtZip = new JTextField(20);
 		c.gridx = 1;
 		c.gridy = 3;
 		pnlNewLocation.add(txtZip, c);
-		
+
 		JLabel lblZipErr = new JLabel();
 		lblZipErr.setForeground(Color.RED);
 		c.gridx = 2;
 		c.gridy = 3;
 		pnlNewLocation.add(lblZipErr, c);
-		
-		int result = JOptionPane.showOptionDialog(null, new Object[] {pnlNewLocation}, "New Location", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-		
+
+		int result = JOptionPane.showOptionDialog(null, new Object[] { pnlNewLocation }, "New Location",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
 		if (result == 0) {
 			String name = txtName.getText();
-			//double rate = Double.parseDouble(txtRate.getText());
+			// double rate = Double.parseDouble(txtRate.getText());
 			int vehicles = Integer.parseInt(txtVehicle.getText());
 			int id = locationList.size() + 1;
 			int zip = Integer.parseInt(txtZip.getText());
-			
+
 			RentalLocations loc = new RentalLocations(name, vehicles, id, zip);
-			
+
 			list.add(loc);
 			updateTable(tblLocations, locationList);
 		}
 	}
-	
+
 	private void updateTable(JTable table, List<RentalLocations> list) {
-		String[] columns = {"ID", "Name", "ZIP Code", "Rented Vehicles", "Avail. Vehicles", "Daily Rate"};
+		String[] columns = { "ID", "Name", "ZIP Code", "Rented Vehicles", "Avail. Vehicles", "Daily Rate" };
 		Object[][] data = new Object[list.size()][6];
-		
+
 		for (int i = 0; i < list.size(); i++) {
 			data[i][0] = list.get(i).getId();
 			data[i][1] = list.get(i).getName();
@@ -224,7 +232,48 @@ public class RentalManagementApp {
 			data[i][4] = list.get(i).availableVehicles();
 			data[i][5] = list.get(i).getRates();
 		}
+
+		DefaultTableModel dtm = new DefaultTableModel(data, columns);
+		table.setModel(dtm);
+	}
+
+	private void availableV(JTable table, List<RentalLocations> list) {
+		String[] columns = { "ID", "Name", "Available Vehicles" };
+		Object[][] data = new Object[list.size()][3];
+
+		for (int i = 0; i < list.size(); i++) {
+			data[i][0] = list.get(i).getId();
+			data[i][1] = list.get(i).getName();
+			data[i][2] = list.get(i).availableVehicles();
+		}
+		DefaultTableModel dtm = new DefaultTableModel(data, columns);
+		table.setModel(dtm);
+	}
+
+	private void ratesByLoc(List<RentalLocations> list) {
+		String name = JOptionPane.showInputDialog(frmRentalLocationManager, "Input location name");
+		updateTable(tblLocations, locationList, name);
+	}
+	//Overloaded for location name input
+	private void updateTable(JTable table, List<RentalLocations> list, String name) {
+		String[] columns = { "ID", "Name", "ZIP Code", "Rented Vehicles", "Avail. Vehicles", "Daily Rate" };
+		List<RentalLocations> locs = new ArrayList<>();
 		
+		for (RentalLocations r : list) {
+			if (r.getName().contentEquals(name)) {
+				locs.add(r);
+			}
+		}
+		
+		Object[][] data = new Object[locs.size()][6];
+		for (int i = 0; i < locs.size(); i++) {
+			data[i][0] = locs.get(i).getId();
+			data[i][1] = locs.get(i).getName();
+			data[i][2] = locs.get(i).getZip();
+			data[i][3] = locs.get(i).getRentedVehicles();
+			data[i][4] = locs.get(i).availableVehicles();
+			data[i][5] = locs.get(i).getRates();
+		}
 		DefaultTableModel dtm = new DefaultTableModel(data, columns);
 		table.setModel(dtm);
 	}
