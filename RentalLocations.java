ec265fba4faea5a9b4104559a8775d6b9810ac42
FreddySig <60980367+FreddySig@users.noreply.github.com>
2020-02-12 10:20:30 -0800

    Update RentalLocations.java

--- RentalLocations.java
+++ RentalLocations.java
@@ -3,13 +3,17 @@ package IJS_Sprint1;
 import java.text.NumberFormat;
 
 public class RentalLocations {
-	private String name; //rental location name,cities for locations
-	private double dailyRate;
+	private String name; // rental location name,cities for locations
+	private int id;
+
 	private final int vehicles = 50;
 	private int rentedVehicles;
-	private int id;
-	
-	//total vehicles  = 50
+
+	private double dailyRate;
+	private int zip;
+	private double discount;
+
+	// total vehicles = 50
 	public RentalLocations(String name, double dailyRate, int rentedVehicles, int id, int zip) {
 		this.name = name;
 		this.dailyRate = dailyRate;
@@ -17,6 +21,7 @@ public class RentalLocations {
 		this.id = id;
 		this.zip = zip;
 	}
+
 	/**
 	 * @return the id
 	 */
@@ -24,38 +29,27 @@ public class RentalLocations {
 		return id;
 	}
 
-	private int zip;
-	private double days;
-	/**
-	 * @return the days
-	 */
-	public double getDays() {
-		return days;
-	}
-	/**
-	 * @param days the days to set
-	 */
-	public void setDays(double days) {
-		this.days = days;
-	}
 	/**
 	 * @return the zip
 	 */
 	public int getZip() {
 		return zip;
 	}
+
 	/**
 	 * @param zip the zip to set
 	 */
 	public void setZip(int zip) {
 		this.zip = zip;
 	}
+
 	/**
 	 * @return the discount
 	 */
 	public double getDiscount() {
 		return discount;
 	}
+
 	/**
 	 * @param discount the discount to set
 	 */
@@ -63,71 +57,14 @@ public class RentalLocations {
 		this.discount = discount;
 	}
 
-	private double discount;
-
-	
-	
-	
-		
-	/* JEFF LOOK HERE FOR LOGIC
-	 * Scanner sc = new Scanner(System.in);
-	 * System.out.println("how many days would you like to rent a vehicle?"); days =
-	 * sc.nextInt(); System.out.println("Please input zip of rental location: ");
-	 * zip = sc.nextInt();
-	 */
-		
-	public double getRates(double dailyRate) { 
-		if ((days > 0) && (days < 5)) {
-			if ((zip >= 98001) && (zip <= 98100)) {
-				dailyRate = 27.50;				
-			}
-			if ((zip >= 98201) && (zip <= 98300)) {
-				dailyRate = 22.49;
-			}
-			if ((zip >= 98301) && (zip <= 98400)) {
-				dailyRate = 31.00;
-			}
-			if ((zip >= 98401) && (zip <= 98500)) {
-				dailyRate = 12.25;
-			}
-		}
-		if (days >= 5 ) { // weekly rate
-			if ((zip >= 98000) && (zip <= 99000)) {
-				discount = 0.2;
-				dailyRate = 27.50;				
-				dailyRate = (dailyRate * discount) + dailyRate;
-				
-			}
-			if (zip == 2) {
-				dailyRate = 22.49;
-				discount = 0.2;
-				dailyRate = (dailyRate * discount) + dailyRate;
-			
-			}
-			if (zip == 3) {
-				dailyRate = 31.00;
-				discount = 0.2;
-				dailyRate = (dailyRate * discount) + dailyRate;
-				
-			}
-			if (zip == 4) {
-				dailyRate = 12.25;discount = 0.2;
-				dailyRate = (dailyRate * discount) + dailyRate;
-				
-			}
-		}
-		double total = days * dailyRate;
-		System.out.println("the total is: " + NumberFormat.getCurrencyInstance().format(total));
-		return dailyRate;
-//		
-//		}
-	
-	}public int getVehicles() {
+	public int getVehicles() {
 		return vehicles;
 	}
+
 	public int getRentedVehicles() {
 		return rentedVehicles;
 	}
+
 	public void setRentedVehicles(int rentedVehicles) {
 		this.rentedVehicles = rentedVehicles;
 	}
@@ -146,7 +83,7 @@ public class RentalLocations {
 	/**
 	 * @param city the city to set
 	 */
-	public void setName (String z) {
+	public void setName(String z) {
 		this.name = z;
 	}
 
@@ -156,5 +93,34 @@ public class RentalLocations {
 
 	public void setDailyRate(double dailyRate) {
 		this.dailyRate = dailyRate;
-	}	
+	}
+
+	/*
+	 * JEFF LOOK HERE FOR LOGIC Scanner sc = new Scanner(System.in);
+	 * System.out.println("how many days would you like to rent a vehicle?"); days =
+	 * sc.nextInt(); System.out.println("Please input zip of rental location: ");
+	 * zip = sc.nextInt();
+	 */
+
+	public double getRates(double dailyRate) {
+		if ((zip >= 98001) && (zip <= 98100)) {
+			dailyRate = 27.50;
+		}
+		if ((zip >= 98201) && (zip <= 98300)) {
+			dailyRate = 22.49;
+		}
+		if ((zip >= 98301) && (zip <= 98400)) {
+			dailyRate = 31.00;
+		}
+		if ((zip >= 98401) && (zip <= 98500)) {
+			dailyRate = 12.25;
+		}
+
+		double total = rentedVehicles * dailyRate; //May 
+		System.out.println("the total is: " + NumberFormat.getCurrencyInstance().format(total));
+		return dailyRate;
+//		
+//		}
+
+	}
 }
