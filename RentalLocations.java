8ca8e2aca4cc7cd90fe838cd231849d6439d070a
megcagle <60980598+megcagle@users.noreply.github.com>
2020-02-12 09:59:18 -0800

    Update RentalLocations.java

--- RentalLocations.java
+++ RentalLocations.java
@@ -61,49 +61,22 @@ public class RentalLocations {
 	 */
 		
 	public double getRates(double dailyRate) { 
-		if ((days > 0) && (days < 5)) {
-			if ((zip >= 98001) && (zip <= 98100)) {
-				dailyRate = 27.50;				
-			}
-			if ((zip >= 98201) && (zip <= 98300)) {
-				dailyRate = 22.49;
-			}
-			if ((zip >= 98301) && (zip <= 98400)) {
-				dailyRate = 31.00;
-			}
-			if ((zip >= 98401) && (zip <= 98500)) {
-				dailyRate = 12.25;
-			}
+		if ((zip >= 98001) && (zip <= 98100)) {
+			dailyRate = 27.50;				
 		}
-		if (days >= 5 ) { // weekly rate
-			if ((zip >= 98000) && (zip <= 99000)) {
-				discount = 0.2;
-				dailyRate = 27.50;				
-				dailyRate = (dailyRate * discount) + dailyRate;
-				
-			}
-			if (zip == 2) {
-				dailyRate = 22.49;
-				discount = 0.2;
-				dailyRate = (dailyRate * discount) + dailyRate;
-			
-			}
-			if (zip == 3) {
-				dailyRate = 31.00;
-				discount = 0.2;
-				dailyRate = (dailyRate * discount) + dailyRate;
-				
-			}
-			if (zip == 4) {
-				dailyRate = 12.25;discount = 0.2;
-				dailyRate = (dailyRate * discount) + dailyRate;
-				
-			}
+		if ((zip >= 98201) && (zip <= 98300)) {
+			dailyRate = 22.49;
 		}
-		total = days * dailyRate;
-		System.out.println("the total is: " + NumberFormat.getCurrencyInstance().format(total));
-		return dailyRate;
-//		
+		if ((zip >= 98301) && (zip <= 98400)) {
+			dailyRate = 31.00;
+		}
+		if ((zip >= 98401) && (zip <= 98500)) {
+			dailyRate = 12.25;
+		}
+		total = rentedVehicles * dailyRate;
+		System.out.println("the total daily revenue is: " + NumberFormat.getCurrencyInstance().format(total));
+		return total;
+	}
 //		}
 	
 	}public int getVehicles() {
