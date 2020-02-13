e224534259cd11fbedd51f2033f1d581e6dedd57
megcagle <60980598+megcagle@users.noreply.github.com>
2020-02-12 16:29:43 -0800

    Update RentalLocations.java
    added vehicle rate based on type

--- RentalLocations.java
+++ RentalLocations.java
@@ -13,6 +13,7 @@ public class RentalLocations implements Serializable {
 	private final int vehicles = 50;
 	private int rentedVehicles;
 
+	private String type;
 	private double dailyRate;
 	private int zip;
 
@@ -86,35 +87,101 @@ public class RentalLocations implements Serializable {
 	}
 
 	public double getRates() {
-		if ((zip >= 98001) && (zip <= 98100)) {
-			dailyRate = 27.50;
+		if (type.equalsIgnoreCase("car")) {	
+			if ((zip >= 98001) && (zip <= 98100)) {
+				dailyRate = 27.50;
+			}
+			if ((zip >= 98101) && (zip <= 98200)) {
+				dailyRate = 22.49;
+			}
+			if ((zip >= 98201) && (zip <= 98300)) {
+				dailyRate = 31.00;
+			}
+			if ((zip >= 98301) && (zip <= 98400)) {
+				dailyRate = 12.25;
+			}
+			if ((zip >= 98401) && (zip <= 98500)) {
+				dailyRate = 75.24;
+			}
+			if ((zip >= 98501) && (zip <= 98600)) {
+				dailyRate = 34.99;
+			}
+			if ((zip >= 98601) && (zip <= 98700)) {
+				dailyRate = 28.99;
+			}
+			if ((zip >= 98701) && (zip <= 98800)) {
+				dailyRate = 33.00;
+			}
+			if ((zip >= 98801) && (zip <= 98900)) {
+				dailyRate = 19.99;
+			}
+			if ((zip >= 98901) && (zip <= 99000)) {
+				dailyRate = 9.99;
+			}
 		}
-		if ((zip >= 98101) && (zip <= 98200)) {
-			dailyRate = 22.49;
+		if (type.equalsIgnoreCase("truck")) {	
+			if ((zip >= 98001) && (zip <= 98100)) {
+				dailyRate = 55.00;
+			}
+			if ((zip >= 98101) && (zip <= 98200)) {
+				dailyRate = 44.98;
+			}
+			if ((zip >= 98201) && (zip <= 98300)) {
+				dailyRate = 61.49;
+			}
+			if ((zip >= 98301) && (zip <= 98400)) {
+				dailyRate = 24.49;
+			}
+			if ((zip >= 98401) && (zip <= 98500)) {
+				dailyRate = 98.74;
+			}
+			if ((zip >= 98501) && (zip <= 98600)) {
+				dailyRate = 69.99;
+			}
+			if ((zip >= 98601) && (zip <= 98700)) {
+				dailyRate = 56.49;
+			}
+			if ((zip >= 98701) && (zip <= 98800)) {
+				dailyRate = 62.79;
+			}
+			if ((zip >= 98801) && (zip <= 98900)) {
+				dailyRate = 27.99;
+			}
+			if ((zip >= 98901) && (zip <= 99000)) {
+				dailyRate = 28.99;
+			}
 		}
-		if ((zip >= 98201) && (zip <= 98300)) {
-			dailyRate = 31.00;
-		}
-		if ((zip >= 98301) && (zip <= 98400)) {
-			dailyRate = 12.25;
-		}
-		if ((zip >= 98401) && (zip <= 98500)) {
-			dailyRate = 75.24;
-		}
-		if ((zip >= 98501) && (zip <= 98600)) {
-			dailyRate = 34.99;
-		}
-		if ((zip >= 98601) && (zip <= 98700)) {
-			dailyRate = 28.99;
-		}
-		if ((zip >= 98701) && (zip <= 98800)) {
-			dailyRate = 33.00;
-		}
-		if ((zip >= 98801) && (zip <= 98900)) {
-			dailyRate = 19.99;
-		}
-		if ((zip >= 98901) && (zip <= 99000)) {
-			dailyRate = 9.99;
+		if (type.equalsIgnoreCase("van")) {	
+			if ((zip >= 98001) && (zip <= 98100)) {
+				dailyRate = 37.50;
+			}
+			if ((zip >= 98101) && (zip <= 98200)) {
+				dailyRate = 32.49;
+			}
+			if ((zip >= 98201) && (zip <= 98300)) {
+				dailyRate = 41.00;
+			}
+			if ((zip >= 98301) && (zip <= 98400)) {
+				dailyRate = 22.25;
+			}
+			if ((zip >= 98401) && (zip <= 98500)) {
+				dailyRate = 85.24;
+			}
+			if ((zip >= 98501) && (zip <= 98600)) {
+				dailyRate = 44.99;
+			}
+			if ((zip >= 98601) && (zip <= 98700)) {
+				dailyRate = 38.99;
+			}
+			if ((zip >= 98701) && (zip <= 98800)) {
+				dailyRate = 43.00;
+			}
+			if ((zip >= 98801) && (zip <= 98900)) {
+				dailyRate = 29.99;
+			}
+			if ((zip >= 98901) && (zip <= 99000)) {
+				dailyRate = 19.99;
+			}
 		}
 
 		return dailyRate;
