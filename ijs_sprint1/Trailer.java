2ad845f05467484834cecb75129020088e70f6d0
148646 <148646@G917LLAX.ustr.com>
2020-02-13 10:08:27 -0800

    alalla

--- ijs_sprint1/Trailer.java
+++ ijs_sprint1/Trailer.java
@@ -1,8 +1,17 @@
 package ijs_sprint1;
 
-public class Trailer {
+public class Trailer extends Vehicles {
 	int trailerId;
 	String type;
 	String trailerStatus;
+	
+	public Trailer(int trailerId, String type, String trailerStatus) {
+		super();
+		this.trailerId = trailerId;
+		this.type = type;
+		this.trailerStatus = trailerStatus;
+	}
+	
+	
 	
 }
